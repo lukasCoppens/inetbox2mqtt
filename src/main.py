c66dcf9672dfe8a9d798263939fa71a7b6b008f8
Lukas Coppens <lukas.coppens@be-mobile.com>
2026-02-01 17:48:36 +0100

    fix tree refs

--- src/main.py
+++ src/main.py
@@ -45,7 +45,7 @@ if (w.run_mode() > 1):
     import mip
     import time
     try:
-        mip.install("github:mc0110/inetbox2mqtt/src/" + UPDATE, target = "/")
+        mip.install("github:lukasCoppens/inetbox2mqtt/src/" + UPDATE, target = "/")
     except:
         import machine
         machine.reset()            
